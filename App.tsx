689f51142924f62fd649f62203e4dd997bbbb10b
Ceagah <carlosceagah@gmail.com>
2024-12-19 14:03:43 -0300

    feat: implement clerk and token cache on route file,

--- App.tsx
+++ App.tsx
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
+import { Archivo_200ExtraLight, Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold, Archivo_700Bold, Archivo_800ExtraBold, Archivo_900Black, useFonts } from "@expo-google-fonts/archivo";
+import { ActivityIndicator } from "react-native";
+import Routes from './src/presentation/routes';
 
 export default function App() {
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+  const [loaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_200ExtraLight,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+    Archivo_700Bold,
+    Archivo_800ExtraBold,
+    Archivo_900Black
+  })
+
+  if(!loaded) {
+    return <ActivityIndicator />
+  }
+
+  if (!publishableKey) {
+    throw new Error( 
+      "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ClerkProvider publishableKey={publishableKey}>
+      <ClerkLoaded>
+        <Routes />
+      </ClerkLoaded>
+    </ClerkProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
