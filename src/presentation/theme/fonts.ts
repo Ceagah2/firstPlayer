99796ded53a24c2d2146d8772315705450b8c245
Ceagah <carlosceagah@gmail.com>
2024-12-19 14:04:00 -0300

    style: create colors font and sizes tokens

---
+++ src/presentation/theme/fonts.ts
@@ -0,0 +1,11 @@
+export const fonts = {
+  archivo: {
+    200: 'Archivo_200ExtraLight',
+    400: 'Archivo_400Regular',
+    500: 'Archivo_500Medium',
+    600: 'Archivo_600SemiBold',
+    700: 'Archivo_700Bold',
+    800: 'Archivo_800ExtraBold',
+    900: 'Archivo_900Black'
+  }
+}
---
+++ src/presentation/theme/sizes.ts
@@ -0,0 +1,13 @@
+export const sizes = {
+  subtitle: "20px",
+  h5: "24px",
+  h4: "32px",
+  h3: "44px",
+  h2: "62px",
+  h1: "80px",
+  body1: "18px",
+  body2: "16px",
+  body3: "14px",
+  caption: "12px",
+  button: "20px",
+}
